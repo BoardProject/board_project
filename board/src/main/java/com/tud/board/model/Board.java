3b639d1b4cc00a271f5f38cbd530d9791548dc26
dongcholes <dongeunleee@gmail.com>
2021-09-15 16:45:02 +0900

    Add entity class

--- board/src/main/java/com/tud/board/model/Board.java
+++ board/src/main/java/com/tud/board/model/Board.java
@@ -1,9 +1,18 @@
 package com.tud.board.model;
 
+
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +24,7 @@ import lombok.NoArgsConstructor;
 @Builder 			// builder 만들어준다.
 @NoArgsConstructor 	// 파라미터 없는 생성자 만들어준다.
 @AllArgsConstructor // 모든 파라미터 대체해주는 생성자 만들어준다.
-@Entity				// DB 바라보는 클래
+@Entity				// DB 바라보는 클래스 
 public class Board {
 	
 	
@@ -23,5 +32,20 @@ public class Board {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 	//auto_increment
 	private int id;
 	
-
+	
+	@Column(nullable = false, length = 100 )
+	private String title;
+	
+	
+	@Lob
+	private String content;
+	
+	
+	
+	@ColumnDefault("0")
+	private int count;
+	
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
