edd925807d1ebc12d3b0797818aa42f9955dab38
dongcholes <dongeunleee@gmail.com>
2021-09-16 13:36:01 +0900

    Add list data to main ui

--- board/src/main/java/com/tud/board/controller/BoardController.java
+++ board/src/main/java/com/tud/board/controller/BoardController.java
@@ -1,15 +1,24 @@
 package com.tud.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import com.tud.board.service.BoardService;
+
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
+	
 	@GetMapping({ "", "/" })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.list()); // (key값,게시글목록) 글목록 들고 오기 
 		return "list";
 	}
 	
