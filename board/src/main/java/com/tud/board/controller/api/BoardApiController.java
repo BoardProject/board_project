9cead6ca6a60072fde7a5e710189f04e03704524
dongcholes <dongeunleee@gmail.com>
2021-09-16 21:31:54 +0900

    Add  deleteMapping

--- board/src/main/java/com/tud/board/controller/api/BoardApiController.java
+++ board/src/main/java/com/tud/board/controller/api/BoardApiController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,11 +37,13 @@ public class BoardApiController {
 //	}
 	
 	
-//	// 게시글 삭제 요청 
-//	@DeleteMapping({ "/delete" })
-//	public String delete() {
-//		return ;
-//	}
+	// 게시글 삭제 요청 
+	@DeleteMapping({ "/api/board/{id}" })
+	public ResponseDto<Integer> deleteByID(@PathVariable int id) {
+		boardService.delete(id);
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
 
 
 }
