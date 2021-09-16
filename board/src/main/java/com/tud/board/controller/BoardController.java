7d24aa48bb6901c51a74907d777fe8957a6f8b7b
dongcholes <dongeunleee@gmail.com>
2021-09-16 16:35:43 +0900

    Add list and paging

--- board/src/main/java/com/tud/board/controller/BoardController.java
+++ board/src/main/java/com/tud/board/controller/BoardController.java
@@ -1,13 +1,23 @@
 package com.tud.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tud.board.dto.ResponseDto;
+import com.tud.board.model.Board;
 import com.tud.board.service.BoardService;
+
 
 @Controller
 public class BoardController {
@@ -16,22 +26,32 @@ public class BoardController {
 	private BoardService boardService;
 	
 	
+	// 메인 리스트 페이지 요청 
 	@GetMapping({ "", "/" })
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.list()); // (key값,게시글목록) -> (list페이지로 boards가 날라감, 글목록 들고 오기)
+	public String index(Model model, @PageableDefault(size=5, sort="id",direction = Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("boards", boardService.list(pageable)); // (key값,게시글목록) -> (list페이지로 boards가 날라감, 글목록 들고 오기)
 		return "list";
 	}
 	
 	
+	
+	// 게시물 작성 페이지 접속  
 	@GetMapping({ "/writeForm" })
 	public String writeForm() {
 		return "board/writeForm"; // .jsp
 	}
 	
+	
+	
 
-	@GetMapping({ "/edit" })
-	public String edit() {
+	// 게시물 수정 페이지 접속 
+	@GetMapping({ "/editForm" })
+	public String editForm() {
 		return "editForm";
 	}
+	
+	
+
+	
 
 }
